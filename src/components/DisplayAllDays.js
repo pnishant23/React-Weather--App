ea4cfdf7188ca1f3faf6818330a779f05558f5b8
pnishant23 <pnishant2323@gmail.com>
2021-11-02 16:45:29 +0530

    2 files modified

--- src/components/DisplayAllDays.js
+++ src/components/DisplayAllDays.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
 export default function DisplayAllDays(value) {
+  console.log(value);
   return (
     <>
-      <div className="day">
-        <div id="temp">{value.sunri}</div>
-        <div className="icon">icon</div>
-        <div>day</div>
-        <div>23/03</div>
-        <div>Rain</div>
-      </div>
+      {value ? (
+        <div className="day">
+          <div id="temp">{value.pressure}</div>
+          <div className="icon">icon</div>
+          <div>day</div>
+          <div>23/03</div>
+          <div>
+            {value.weather.map((x) => {
+              return x.main;
+            })}
+          </div>
+        </div>
+      ) : (
+        <div className="loading-cnt">
+          <div className="loading">loading..</div>
+        </div>
+      )}
     </>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
+import axios from 'axios';
 
 export default function Search() {
+  const [cityName, setCityName] = useState();
+  const [data, setData] = useState();
+  const API = '9fea503e8177fd247f33fbb6357119d3';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API}`;
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [cityName]);
+
+  function handleChange(e) {
+    e.preventDefault;
+    setCityName(e.target.value);
+  }
+
+  console.log(data);
   return (
     <>
       <Navbar />
+      <h3>Search weather of any city</h3>
+      <div className="search">
+        <div>
+          <input
+            type="text"
+            id="search"
+            value={cityName}
+            onChange={handleChange}
+          />
+          <label for="search">
+            <i className="fas fa-search"></i>
+          </label>
+        </div>
+        {data ? (
+          <div className="result">
+            <header>
+              <h1>
+                {data.name},{data.sys.country}
+              </h1>
+            </header>
+
+            <div className="ti">
+              <div id="temp">{data.main.temp}</div>
+              <div id="icon">
+                {data.weather.map((x) => {
+                  return (
+                    <img
+                      src={`http://openweathermap.org/img/wn/${x.icon}.png`}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+
+            <div id="para">
+              <p>humidity: {data.main.humidity}</p>
+              <p>pressure: {data.main.pressure}hpa</p>
+              <p>wind speed: {data.wind.speed}</p>
+              <p>visibility: {data.main.visibility}</p>
+              <p>: 60hpa</p>
+            </div>
+          </div>
+        ) : (
+          <div className="loadin-cnt">
+            <div className="loading">loading....</div>
+          </div>
+        )}
+      </div>
     </>
   );
 }
